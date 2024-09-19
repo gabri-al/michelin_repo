00a2b82f9635a142cec6fe4771673d0500b8a1b6
gabri-al <gabri-al>
2024-09-19 11:03:56 +0000

    Initial Upload

---
+++ 00_InfraSetup/cluster_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+/databricks/python/bin/pip install mlflow==2.11.1
+/databricks/python/bin/pip install pandas
+/databricks/python/bin/pip install Flask
+/databricks/python/bin/pip install plotly==5.24.1
+/databricks/python/bin/pip install dash
+/databricks/python/bin/pip install dash-bootstrap-components
+/databricks/python/bin/pip install dash-core-components
+/databricks/python/bin/pip install dash-html-components
+/databricks/python/bin/pip install dash-table
