ede6f5baed89d0fb55d452aedfa48cc50978dda9
Gabriele Albini <gabri.albini@gmail.com>
2024-10-28 09:43:01 +0100

    Info page improved

--- 02_WebApp/pages/info.py
+++ 02_WebApp/pages/info.py
@@ -16,7 +16,7 @@ layout = dbc.Container([
     ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
     dbc.Row([
         dbc.Col([
-            html.H2("Welcome to this Michelin Restaurants WebApp!", className='titles-h2')
+            html.H1("Welcome to the Michelin Restaurants WebApp!", className='titles-h1-info')
         ], width=12)
     ], className = 'info-title-row'),
 
@@ -26,32 +26,55 @@ layout = dbc.Container([
     dbc.Row([
         dbc.Col([
             html.Div([
-                html.H2("Search Mode", className='titles-h2'),
+                html.Br(),
+                html.P([
+                    "This WebApp is designed to showcase several features of Plotly Dash, primarily including: Maplibre Maps, integration with third-party APIs, and Reports.",
+                    html.Br(),
+                    "The data used in the app is sourced from the 2021 ",
+                    html.A('Michelin Restaurants dataset.', href='https://www.kaggle.com/datasets/ngshiheng/michelin-guide-restaurants-2021'),
+                    html.Br(),
+                    "For further information, please check out the ",
+                    html.A('GitHub repository', href=''),
+                    " or ",
+                    html.A('contact the author.', href='https://www.linkedin.com/in/gabriele-albini-85100549/'),
+                    html.Br(),html.Br(),
+                    "The app consists of two pages that share the same filters but provide different functionalities:",
+                    html.Br()
+                ], className = 'info-p'),
+                html.Br(),
+                html.H2("Search Page", className='titles-h2'),
                 html.P([
                     "The Search page allows users to look for restaurants based on a search query and apply optional filters.",
                     html.Br(),
                     "The search query will be embedded via API, using the ",
                     html.A('bge-base-en-v1.5 model', href='https://huggingface.co/BAAI/bge-base-en-v1.5'),
                     html.Br(),
-                    "Results, displayed on the map and in the results tiles, are ranked by the cosine similarity of the restaurant descriptions."
-                ], className = 'charts-p'),
-
-                html.H2("Insights Mode", className='titles-h2'),
+                    "Results, displayed on the map and in the results tiles, are ranked by the cosine similarity of the restaurant descriptions.",
+                    html.Br(),html.Br(),html.Br(),
+                ], className = 'info-p'),
+                html.H2("Insights Page", className='titles-h2'),
                 html.P([
-                    "The Insights page shows some pre-built figures based on the ",
-                    html.A('Michelin Restaurants dataset.', href='https://www.kaggle.com/datasets/ngshiheng/michelin-guide-restaurants-2021'),
+                    "The Insights page presents pre-built maps and charts based on KPIs that can be extracted from the dataset.",
                     html.Br(),
                     "Insights are organized in the following sections:",
                     html.Br(),
-                    "Restaurants by Country",
+                    html.Ul([
+                        html.Li([
+                            "Restaurants by Country",
+                        ]),
+                        html.Li([
+                            "Insights by Award, showing: stars and star propensity by country",
+                        ]),
+                        html.Li([
+                            "Insights by Cuisines, showing: Cuisines by Country, Popular Cuisines, Star Propensity by Cuisine",
+                        ]),
+                        html.Li([
+                            "Insights by Price, showing: Price Score by Country, Price Category by Cuisine, Price Category by Award",
+                        ]),
+                    ]),
                     html.Br(),
-                    "Insights by Award, showing: stars by countryand star propensity by country",
-                    html.Br(),
-                    "Insights by Cuisines, showing: Cuisines by Country, Popular Cuisines, Star Propensity by Cuisine",
-                    html.Br(),
-                    "Insights by Price, showing: Price Score by Country, Price Category by Cuisine, Price Category by Award"
-                ], className = 'charts-p')
-            ], className = 'chart-div')
+                ], className = 'info-p')
+            ], className = 'info-div')
         ], width = 12)
     ])
 
